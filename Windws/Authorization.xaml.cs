42a214e7b4e865f5123d4c5b053f9c859c832e84
diorblack <45602451+claudy1337@users.noreply.github.com>
2022-11-20 05:20:22 +0300

    Добавьте файлы проекта.

---
+++ Windws/Authorization.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFUIKitProfessional.Windws
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                    this.DragMove();
+            }
+            catch (System.InvalidOperationException)
+            {
+                return;
+            }
+        }
+
+        private void txtRegistration_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Registration reg = new Registration();
+            reg.Show();
+            this.Close();
+        }
+
+        private void btnAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtLogin.Text) || string.IsNullOrWhiteSpace(txtPassword.Password))
+            {
+                MessageBox.Show("empty fields!");
+                return;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Windws/Registration.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFUIKitProfessional.Windws
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                    this.DragMove();
+            }
+            catch (System.InvalidOperationException)
+            {
+                return;
+            }
+        }
+
+        private void txtAuth_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Authorization auth = new Authorization();
+            auth.Show();
+            this.Close();
+        }
+
+        private void btnRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPassword.Password) || string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtLogin.Text))
+            {
+                MessageBox.Show("empty fields!");
+                return;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
