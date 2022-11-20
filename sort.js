2f744c6915b1be1be4025338034b43268c91caf9
звук кайфа/diorblack <45602451+claudy1337@users.noreply.github.com>
2022-11-20 05:36:21 +0300

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,2 +1,4 @@
-const sorting_stuff =["value1", "value2", "value3"];
+var sorting_stuff = [ [4, "Trapezium"], [5, "Pentagon"], [3, "Triangle"], [4, "Rectangle"], [4, "Square"] ]; 
+sorting_stuff.sort(function(a, b) { if(a[0] === b[0]) { var x = a[1].toLowerCase(), y = b[1].toLowerCase(); 
+return x < y ? -1 : x > y ? 1 : 0; } return a[0] - b[0]; }); 
 sorting_stuff.sort();
